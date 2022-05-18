afbdeefac15f461fd3d8ebecb0a2b1dead903b84
kenny0707 <100082366+kenny0707@users.noreply.github.com>
2022-05-18 16:58:46 +0800

    Add files via upload

---
+++ main4.cpp
@@ -0,0 +1,28 @@
+#include "mbed.h"
+#include "bbcar.h"
+#include <cstdio>
+
+DigitalOut led1(LED1);
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+DigitalInOut pin10(D10);
+
+BBCar car(pin5, pin6, servo_ticker);
+
+int main() {
+   parallax_ping  ping1(pin10);
+   car.goStraight(100);
+   while(1) {
+        if((float)ping1>50){
+        led1 = 1;
+        printf("%lf\r\n",float(ping1));
+        } 
+        else {
+            led1 = 0;
+            car.stop();
+            break;
+        }
+      ThisThread::sleep_for(1000ms);
+   }
+}
+
